09ff63bf04116e8c251a0567a8dba098d48aa3ab
Vladislav A. Proskurov <rilshok@pm.me>
2021-10-31 03:29:01 +0700

    installation

---
+++ setup.py
@@ -0,0 +1,21 @@
+import runpy
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+name = 'emitime'
+descriprion = 'Emit time from time!'
+version_path = Path(__file__).resolve().parent / name / '__version__.py'
+version = runpy.run_path(version_path)['__version__']
+
+with open('requirements.txt', encoding='utf-8') as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name=name,
+    packages=find_packages(include=(name,)),
+    descriprion=descriprion,
+    version=version,
+    install_requires=requirements,
+    # python_requires='>3.9',
+)
