2a762f1daf97c28cd411f4c4c0971fa495311ff1
Vladislav A. Proskurov <rilshok@pm.me>
2021-11-03 21:09:11 +0700

    strong import

--- emitime/__init__.py
+++ emitime/__init__.py
@@ -1,4 +1,4 @@
 from emitime import parser
-from emitime.date import *
-from emitime.datetime import *
-from emitime.time import *
+from emitime.datetime import DateTime
+from emitime.date import Date, Year, Month, Day
+from emitime.time import Time, Atomic, Seconds, Minutes, Hours, Days, timerange
