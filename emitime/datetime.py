568b6719c7f619dc0c2f4b2f67db8ae9c6b1a17f
Vladislav A. Proskurov <rilshok@pm.me>
2021-11-03 17:16:14 +0700

    mock of date and time property

--- emitime/datetime.py
+++ emitime/datetime.py
@@ -2,6 +2,17 @@ __all__ = [
     'DateTime',
 ]
 
+from emitime import date as d
+from emitime import time as t
+
 class DateTime:
     def __init__(self, *args, **kwargs) -> None:
+        raise NotImplementedError
+
+    @property
+    def date(self) -> d.Date:
+        raise NotImplementedError
+
+    @property
+    def time(self) -> t.Time:
         raise NotImplementedError
