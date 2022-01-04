c5ce8673b982d859cd75c4381d59d984389736eb
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-04 17:28:39 +0700

    + date to string

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,6 +1,7 @@
 from emitime.date import *
 import datetime as dt
 
+
 def test_create_date_by_pydate():
     pyd = dt.date(year=2014, month=3, day=16)
     d = Date(pyd)
@@ -9,3 +10,7 @@ def test_create_date_by_pydate():
     assert d.day == pyd.day
     assert d.pydate == pyd
     assert d == pyd
+
+def test_date_to_str():
+    assert str(Date(year=1992, month=1, day=2)) == '1992-01-02'
+    assert str(Date(year=1993, month=12, day=12)) == '1993-12-12'
