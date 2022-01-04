2dc89abe564989e00989d6baf6f4674becf6804c
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-04 17:32:56 +0700

    + comparison

--- tests/test_date.py
+++ tests/test_date.py
@@ -14,3 +14,15 @@ def test_create_date_by_pydate():
 def test_date_to_str():
     assert str(Date(year=1992, month=1, day=2)) == '1992-01-02'
     assert str(Date(year=1993, month=12, day=12)) == '1993-12-12'
+
+def test_date_comparison():
+    d1 = Date(year=1992, month=1, day=2)
+    d2 = Date(year=1993, month=12, day=12)
+    d3 = Date(year=1993, month=12, day=12)
+    assert d1 < d2 <= d3
+    assert d3 >= d2 > d1
+    assert d3 >= d1
+    assert d1 != d2
+    assert d2 == d3
+    assert d1 < d2.pydate
+    assert d2.pydate > d1
