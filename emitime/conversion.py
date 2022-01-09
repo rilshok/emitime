49d9ea79303a1bdd263ae61468b18c22c5574001
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-10 03:20:35 +0700

    need fix

--- emitime/conversion.py
+++ emitime/conversion.py
@@ -1,8 +1,12 @@
 import datetime as dt
+
+from plum import add_conversion_method
+
 from emitime.date import _string_to_ymd
 
 
 def str_to_datetime(value: str) -> dt.datetime:
+    # FIXME: yyyy-mm-dd^hh:mm[:ss[.ms['μs]]]
     y, m, d = _string_to_ymd(value)
     return dt.datetime(year=y, month=m, day=d)
 
@@ -13,6 +17,7 @@ def datetime_to_str(value: dt.datetime) -> str:
     day = value.day
     string = "{}-{:02}-{:02}".format(int(year), int(month), int(day))
     return string
+
 
 def timedelta_to_str(value: dt.timedelta) -> str:
     sec = value.total_seconds()
@@ -37,19 +42,20 @@ def timedelta_to_str(value: dt.timedelta) -> str:
         tail = f":{s:02}{tail}"
     return f"{sign}{time}{tail}"
 
+
 def str_to_timedelta(value: str) -> dt.timedelta:
     try:
-        sign = 1.
+        sign = 1.0
         d = h = m = s = ms = μs = 0
-        if value.startswith('-'):
-            sign = -1.
+        if value.startswith("-"):
+            sign = -1.0
             value = value[1:]
         if "^" in value:
-            split = value.split('^')
+            split = value.split("^")
             value = split[-1]
             d = int(split[0])
         if "." in value:
-            split = value.split('.')
+            split = value.split(".")
             value = split[0]
             msμs = split[-1]
             if "'" in msμs:
@@ -61,15 +67,23 @@ def str_to_timedelta(value: str) -> dt.timedelta:
         m = int(split[1])
         if len(split) == 3:
             s = int(split[2])
-        assert (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60 and 0 <= ms < 1e3 and 0 <= μs < 1e3)
+        assert (
+            0 <= h < 24
+            and 0 <= m < 60
+            and 0 <= s < 60
+            and 0 <= ms < 1e3
+            and 0 <= μs < 1e3
+        )
     except Exception:
-        need_format = "time part does not match the format [-|+][d^]hh:mm[:ss[.ms['μs]]]"
+        need_format = (
+            "time part does not match the format [-|+][d^]hh:mm[:ss[.ms['μs]]]"
+        )
         raise AssertionError(need_format)
-    total = (86_400*d + 3_600*h + 60*m + s) * 1_000_000  + ms * 1_000 + μs
-    return dt.timedelta(microseconds=sign*total)
+    total = (86_400 * d + 3_600 * h + 60 * m + s) * 1_000_000 + ms * 1_000 + μs
+    return dt.timedelta(microseconds=sign * total)
+
 
 def add_conversion_methods():
-    from plum import add_conversion_method
     add_conversion_method(type_from=str, type_to=dt.datetime, f=str_to_datetime)
     add_conversion_method(type_from=dt.datetime, type_to=str, f=datetime_to_str)
     add_conversion_method(type_from=str, type_to=dt.timedelta, f=str_to_timedelta)
