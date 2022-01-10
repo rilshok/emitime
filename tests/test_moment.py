9e182e21cb11877347bcf4151f1d597a1e38df4b
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-10 16:02:22 +0700

    tests up

--- tests/test_moment.py
+++ tests/test_moment.py
@@ -1,47 +1,59 @@
-from emitime.date import *
 import datetime as dt
 
+from emitime import Moment
+from pytest import mark
 
-def test_create_date_by_pydate():
-    pyd = dt.date(year=2014, month=3, day=16)
-    d = Date(pyd)
-    assert d.year == pyd.year
-    assert d.month == pyd.month
-    assert d.day == pyd.day
-    assert d.pydate == pyd
-    assert d == pyd
 
-def test_create_date_by_string():
-    d = Date('2014-03-16')
-    assert d.year == 2014
-    assert d.month == 3
-    assert d.day == 16
+def test_moment_by_pydate() -> None:
+    d = dt.date(year=2014, month=3, day=16)
+    m = Moment(d)
+    assert m.datetime.year == d.year
+    assert m.datetime.month == d.month
+    assert m.datetime.day == d.day
+    assert m == d
 
-def test_date_to_str():
-    d1 = Date(year=1992, month=1, day=2)
-    d2 = Date(year=1993, month=12, day=12)
-    assert str(d1) == '1992-01-02'
-    assert str(d2) == '1993-12-12'
-    assert str(d1.year) == '1992 year'
-    assert str(d1.month) == 'jan'
-    assert str(d1.month).long == 'January'
-    assert str(d1.day) == '2 day'
-    assert str(d1).ru == '02.01.1992'
-    assert str(d1).eng_long == '2 jan 1992'
 
-def test_date_comparison():
-    d1 = Date(year=1992, month=1, day=2)
-    d2 = Date(year=1993, month=12, day=12)
-    d3 = Date(year=1993, month=12, day=12)
-    assert d1 < d2 <= d3
-    assert d3 >= d2 > d1
-    assert d3 >= d1
-    assert d1 != d2
-    assert d2 == d3
-    assert d1 < d2.pydate
-    assert d2.pydate > d1
+@mark.parametrize(
+    "value",
+    [
+        "2014-03-16",
+        "2014/03/16",
+        "16.03.2014",
+    ],
+)
+def test_moment_by_string(value: str) -> None:
+    m = Moment(value)
+    assert m.datetime.year == 2014
+    assert m.datetime.month == 3
+    assert m.datetime.day == 16
 
-def test_date_arithmetic():
-    pyd1 = dt.date(year=1992, month=1, day=2)
-    pyd2 = dt.date(year=1993, month=12, day=12)
-    # Date(pyd2) - Date(pyd1) ==== pyd2-pyd1
+
+@mark.parametrize(
+    "date,target",
+    [
+        (dt.datetime(1992, 1, 2), "1992-01-02"),
+    ],
+)
+def test_moment_to_string(date: dt.datetime, target: str) -> None:
+    assert Moment(date) == target
+
+
+def test_moment_comparison():
+    m1 = Moment(dt.date(year=1992, month=1, day=2))
+    m2 = Moment(dt.date(year=1993, month=12, day=12))
+    m3 = Moment(dt.date(year=1993, month=12, day=12))
+    assert m1 < m2 <= m3
+    assert m3 >= m2 > m1
+    assert m3 >= m1
+    assert m1 != m2
+    assert m2 == m3
+    assert m1 < m2.datetime
+    assert m2.datetime > m1
+
+
+def test_moment_arithmetic():
+    d1 = dt.date(year=1992, month=1, day=2)
+    d2 = dt.date(year=1993, month=12, day=12)
+    assert Moment(d2) - Moment(d1) == d2 - d1
+    assert Moment(d2) - d1 == d2 - d1
+    assert d2 - Moment(d1) == d2 - d1
