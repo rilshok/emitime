0259a3224fce13f4a53b74b815f7ebf327771101
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-11 12:25:18 +0700

    rm legacy

--- emitime/__init__.py
+++ emitime/__init__.py
@@ -1,5 +1,2 @@
 from emitime import parser
-# from emitime.datetime import DateTime
-# from emitime.date import Date, Year, Month, Day
-# from emitime.time import Time, Atomic, Seconds, Minutes, Hours, Days, timerange
 from emitime.base import Moment, Interval
