e9ba7da841c07d98aadd83391c240f2a57940232
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-30 17:28:51 +0700

    + ram parser

---
+++ emitime/parser/ram.py
@@ -0,0 +1,37 @@
+import pickle
+from typing import Any, Callable, Dict
+
+import xxhash
+from emitime import Interval, Moment
+
+
+class ParserRAM:
+    def __init__(self, factory: Callable):
+        self._mem: Dict[bytes, Any] = dict()
+        self._hash = xxhash.xxh3_128_digest
+        self._make = factory
+
+    def __call__(self, *args, **kwargs) -> Any:
+        dump = pickle.dumps(args) + pickle.dumps(kwargs)
+        hash = self._hash(dump)
+        if hash in self._mem:
+            return self._mem[hash]
+        obj = Moment(*args, **kwargs)
+        self._mem[hash] = obj
+        return obj
+
+
+class MomentParserRAM(ParserRAM):
+    def __init__(self):
+        super().__init__(Moment)
+
+    def __call__(self, *args, **kwargs) -> Moment:
+        return super().__call__(*args, **kwargs)
+
+
+class IntervalParserRAM(ParserRAM):
+    def __init__(self):
+        super().__init__(Interval)
+
+    def __call__(self, *args, **kwargs) -> Interval:
+        return super().__call__(*args, **kwargs)
