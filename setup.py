c7f048d6290b99c02e2de26b8162831b50e3ce01
Vladislav A. Proskurov <rilshok@pm.me>
2022-01-30 16:34:59 +0700

    include stubs

--- setup.py
+++ setup.py
@@ -3,35 +3,38 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
-name = 'emitime'
-descriprion = 'Emit time from any time!'
-author = 'Vladislav A. Proskurov'
-author_email = 'rilshok@pm.me'
+name = "emitime"
+descriprion = "Emit time from any time!"
+author = "Vladislav A. Proskurov"
+author_email = "rilshok@pm.me"
 
-version_path = Path(__file__).resolve().parent / name / '__version__.py'
-version = runpy.run_path(version_path)['__version__']
+version_path = Path(__file__).resolve().parent / name / "__version__.py"
+version = runpy.run_path(version_path)["__version__"]
 
 with open("README.md", "r") as fh:
-	long_description = fh.read()
+    long_description = fh.read()
 
-with open('requirements.txt', encoding='utf-8') as file:
+with open("requirements.txt", encoding="utf-8") as file:
     requirements = file.read().splitlines()
 
 setup(
     name=name,
-    author=author,
-    author_email=author_email,
     descriprion=descriprion,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rilshok/emitime",
-    packages=find_packages(include=(name,)),
+    author=author,
+    author_email=author_email,
     version=version,
+    url="https://github.com/rilshok/emitime",
+    packages=[name],
+    package_data={name: ["py.typed", *map(lambda x: x.name, Path("emitime").rglob("*.pyi"))]},
     install_requires=requirements,
-    	classifiers=[
-		"Programming Language :: Python :: 3.6",
-		"License :: OSI Approved :: MIT License",
-		"Operating System :: OS Independent",
-	],
-    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
 )
