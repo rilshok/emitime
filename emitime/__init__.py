d8f15c4ceb4c122b91d80df099e947d474cde62e
Vladislav A. Proskurov <rilshok@pm.me>
2022-02-01 12:51:07 +0700

    up v

--- emitime/__init__.py
+++ emitime/__init__.py
@@ -1,2 +1,1 @@
 from emitime.base import Interval, Moment
-from emitime import parser
--- emitime/__init__.pyi
+++ emitime/__init__.pyi
@@ -1,2 +1,1 @@
-from emitime import parser as parser
 from emitime.base import Interval as Interval, Moment as Moment
