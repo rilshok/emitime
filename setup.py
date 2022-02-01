35a683e9f08f6d33b304055dae5e46417cfea3f9
Vladislav A. Proskurov <rilshok@pm.me>
2022-02-01 12:49:35 +0700

    classifiers

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ name = "emitime"
 descriprion = "Emit time from any time!"
 author = "Vladislav A. Proskurov"
 author_email = "rilshok@pm.me"
+classifiers = [
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
 
 version_path = Path(__file__).resolve().parent / name / "__version__.py"
 version = runpy.run_path(version_path)["__version__"]
@@ -26,20 +33,16 @@ setup(
     descriprion=descriprion,
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
     author=author,
     author_email=author_email,
     version=version,
     url="https://github.com/rilshok/emitime",
     packages=[name],
+    include_package_data=True,
     package_data=package_data,
     zip_safe=False,
     install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    classifiers=classifiers,
     python_requires=">=3.6",
 )
