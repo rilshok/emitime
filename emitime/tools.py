08e2dbd5b9594fa9ef72922cc839bffec8115afa
Vladislav A. Proskurov <rilshok@pm.me>
2022-05-20 18:46:06 +0400

    faster momentrange

--- emitime/tools.py
+++ emitime/tools.py
@@ -3,19 +3,21 @@ __all__ = ("momentrange",)
 from .base import Interval, Moment
 import typing as tp
 
+
 def momentrange(start, stop, step) -> tp.Iterable[Moment]:
-    start = Moment(start)
-    stop = Moment(stop)
-    step = Interval(step)
+    start = Moment(start).datetime
+    stop = Moment(stop).datetime
+    step = Interval(step).timedelta
+    # return start, stop, step
     assert stop != start
     side = True
-    if step > "00:00:00.001":
+    if step.total_seconds() > 0.001:
         assert start < stop
-    elif step < "-00:00:00.001":
+    elif step.total_seconds() < -0.001:
         side = False
         assert stop < start
     else:
         raise ValueError(step)
     while start < stop if side else start > stop:
-        yield start
+        yield Moment(start)
         start = start + step
